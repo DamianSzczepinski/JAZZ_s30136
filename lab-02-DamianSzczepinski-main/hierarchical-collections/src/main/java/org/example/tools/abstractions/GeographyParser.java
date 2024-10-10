f0997cf68f4bef414cd137c2315410129abc1c70
DamianSzczepinski <153921973+DamianSzczepinski@users.noreply.github.com>
2024-10-10 20:55:34 +0200

    Update GeographyParser.java

--- lab-02-DamianSzczepinski-main/hierarchical-collections/src/main/java/org/example/tools/abstractions/GeographyParser.java
+++ lab-02-DamianSzczepinski-main/hierarchical-collections/src/main/java/org/example/tools/abstractions/GeographyParser.java
@@ -2,23 +2,22 @@ package org.example.model.abstraction;
 
 import org.example.model.Geography;
 
-import java.util.Objects;
-
-import static java.sql.Types.NULL;
-
 public class GeographyParser implements IParse<Geography> {
+
     @Override
-    public Geography parse(String input) {
-        input = input.trim();
-        String[] tokens = input.split(";");
+    public Geography parse(String data) {
+        String cleanedInput = data.strip();
+        String[] parts = cleanedInput.split(";");
+
         Geography geography = new Geography();
-        geography.setId(Integer.parseInt(tokens[0]));
-        geography.setName(tokens[2]);
-        geography.setType(tokens[1]);
-        geography.setCode(tokens[3]);
-        if (!Objects.equals(tokens[4], "NULL"))
-        {
-            geography.setParentId(Integer.parseInt(tokens[4]));
+        geography.setId(Integer.parseInt(parts[0]));
+        geography.setType(parts[1]);
+        geography.setName(parts[2]);
+        geography.setCode(parts[3]);
+
+        String parentIdValue = parts[4];
+        if (!"NULL".equals(parentIdValue)) {
+            geography.setParentId(Integer.parseInt(parentIdValue));
         }
 
         return geography;
